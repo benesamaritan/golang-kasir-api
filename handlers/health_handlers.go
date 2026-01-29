f29acbf8ff051bca3f31e419905d035eebf7f097
benesamaritan <243715333+benesamaritan@users.noreply.github.com>
2026-01-29 12:56:38 +0700

    tambah contoh env

---
+++ handlers/health_handlers.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func HealthCheckHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{
+			"status":  "OK",
+			"message": "API Berjalan Dengan Sempurna",
+		})
+	}
+}
